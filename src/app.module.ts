a84c856602a2f8653ee7c90f87c7156846a279e6
Sergey <119594843+gordondev@users.noreply.github.com>
2025-08-24 15:49:00 +0500

    move database files to database folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './modules/database/database.module';
 import { TariffsModule } from './modules/tariffs/tariffs.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
@@ -11,7 +10,6 @@ import { ScheduleModule } from '@nestjs/schedule';
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     ScheduleModule.forRoot(),
-    DatabaseModule,
     TariffsModule,
   ],
 })
